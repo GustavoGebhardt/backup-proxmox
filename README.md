58673231f8e80b2b065c4b51085095d47f5102a7
Gustavo Gebhardt <gustavogebhardt@MacBook-Air-de-Gustavo.local>
2024-07-12 17:25:07 -0300

    README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nome do Projeto
+
+Breve descrição do projeto aqui.
+
+## Sumário
+
+- [Instalação](#instalação)
+- [Uso](#uso)
+- [Contribuição](#contribuição)
+- [Licença](#licença)
+
+## Instalação
+
+Descreva como instalar e configurar o projeto localmente. Forneça todos os pré-requisitos necessários, comandos de instalação e configuração.
+
+Exemplo:
+
+```bash
+git clone https://github.com/seu-usuario/seu-projeto.git
+cd seu-projeto
+npm install
--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,5 @@
-import puppeteer from 'puppeteer';
 import sendMessage from './telegram/sendMessage';
 import addRows from './sheets/addRow';
-// Or import puppeteer from 'puppeteer-core';
 
-async function getInfo(){
-    addRows("C5", "Aqui estou!")
-    sendMessage("Hello, World!")
-    
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-
-    // Navigate the page to a URL.
-    await page.goto('https://developer.chrome.com/');
-
-    // Set screen size.
-    await page.setViewport({width: 1080, height: 1024});
-
-    // Type into search box.
-    await page.locator('.devsite-search-field').fill('automate beyond recorder');
-
-    // Wait and click on first result.
-    await page.locator('.devsite-result-item-link').click();
-
-    // Locate the full title with a unique string.
-    const textSelector = await page
-    .locator('text/Customize and automate')
-    .waitHandle();
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-    // Print the full title.
-    console.log('The title of this blog post is "%s".', fullTitle);
-
-    await browser.close();
-}
-
-getInfo()
+addRows("C5", "Aqui estou!")
+sendMessage("Hello, World!")
