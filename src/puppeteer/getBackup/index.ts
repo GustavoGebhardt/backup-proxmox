ffb8b99abda324274ac8a75bab43c03e3577ee28
Gustavo Gebhardt <gustavo.gebhardt@tecnogate.com.br>
2024-07-16 10:23:59 -0300

    Conection proxmox

--- src/puppeteer/getBackup/index.ts
+++ src/puppeteer/getBackup/index.ts
@@ -9,7 +9,7 @@ function delay(time: number) {
 
 async function getBackup(){
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors'] });
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors'] });
     const page = await browser.newPage();
 
     // Navigate the page to a URL.
@@ -19,24 +19,23 @@ async function getBackup(){
     await page.setViewport({width: 1080, height: 1024});
 
     // Type into search box.
-    await page.locator('input[name="username"]').fill('root');
-    await page.locator('input[name="password"]').fill('131328');
+    await page.locator('input[name="username"]').fill(process.env.PROXMOX_USER!);
+    await page.locator('input[name="password"]').fill(process.env.PROXMOX_PASSWORD!);
 
     // Wait and click on first result.
     await page.click('a.x-btn.x-unselectable.x-box-item.x-toolbar-item.x-btn-default-small');
     await delay(1000)
     await page.reload()
-    const element = await page.waitForSelector('::-p-xpath(//*[@id="proxmoxlogo-1018"])');
-    console.log(element)////*[@id="gridview-2566-record-7770"]/tbody/tr/td[6]/div
 
-    // Locate the full title with a unique string.
-    const textSelector = await page
-    .locator('.x-grid-cell-inner')
-    .waitHandle();
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-    // Print the full title.
-    console.log('The title of this blog post is "%s".', fullTitle);
+    await delay(2000)
+    await page.click('::-p-xpath(//*[@id="treeview-1013-record-478"]/tbody/tr/td/div/div[2])')
+    await page.click('::-p-xpath(//*[@id="treeview-1013-record-479"]/tbody/tr/td/div)')
+    await page.click('::-p-xpath(//*[@id="ext-element-229"])')
+    
+    await delay(1000)
+    const backup = await page.$$("::-p-xpath('/html/body/div[2]/div/div/div[3]/div/div/div[3]/div[1]/div[2]/table/tbody/tr/td[6]/div')");
+    const valor = await page.evaluate(el => el.textContent, backup[0]);
+    console.log(valor!.trim());
 
     await browser.close();
 }
--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,25 @@ import getBackup from './puppeteer/getBackup';
 
 //addRows("C5", "Aqui estou!")
 //sendMessage("Hello, World!")
-getBackup()
+
+function main(){
+
+    const date = new Date
+    const options = { timeZone: 'America/Sao_Paulo' };
+    const dateFuso = date.toLocaleString('pt-BR', options);
+    console.log(dateFuso)
+    console.log(parseInt(dateFuso.split(" ")[1].split(":")[0]))
+
+    if(dateFuso.split(" ")[1].split(":")[0] == "05"){
+        try{
+            //getBackup()
+            //throw new Error("Falha no codigo");
+        } catch(erro){
+            sendMessage("ERRO ⚠️\n" + erro + "\nHorario: " + dateFuso.split(" ")[1] + "\nData: " + dateFuso.split(",")[0])
+        }
+    }
+}
+
+main()
+setInterval(() => {main()}, 3600000)
+//86400000
